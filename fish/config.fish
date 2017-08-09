a04ddb72563322bd827398a825938f0fe91fa03f
Yuze Jiang <fortissimoX@gmail.com>
2017-08-09 19:41:42 +0800

    Add fish.config

---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+thefuck --alias | source
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+
+export http_proxy=http://127.0.0.1:1087
+export https_proxy=http://127.0.0.1:1087
