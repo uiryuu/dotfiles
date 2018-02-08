6e4dd9631e4b56425561dbc248a88fc79bf706c4
Yuze Jiang <fortissimoX@gmail.com>
2018-02-07 21:27:39 -0600

    Add .tmux.conf

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,6 @@ thefuck --alias | source
 
 # OPAM configuration
 source /Users/alejx/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
+
+# PATH
+set -gx PATH /Users/alejx/Library/Python/2.7/bin $PATH
