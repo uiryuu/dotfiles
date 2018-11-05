137bb6f23d0ea3f04e611ba3d35bebb04f41138b
Yuze Jiang <fortissimoX@gmail.com>
2018-11-04 18:43:32 -0600

    Move macOS path to config.fish.local

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,1 @@
-# thefuck configuration
-thefuck --alias | source
-[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
-
-# OPAM configuration
-source /Users/alejx/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
-
-# PATH
-set -gx PATH /Users/alejx/Library/Python/2.7/bin $PATH
+source ~/.config/fish/config.fish.local
