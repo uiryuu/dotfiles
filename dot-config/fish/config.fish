558aa9be47b9377751583c86f2a660c405b344a7
Yuze Jiang <i@uiryuu.moe>
2025-07-06 13:22:37 +0900

    Remove install.sh, update README

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -1,14 +1,10 @@
 set -x LC_ALL en_US.UTF-8
-set -x PATH /Library/Tex/texbin /opt/homebrew/bin /opt/homebrew/sbin $PATH
 
 set -gx GPG_TTY (tty)
 
 if status is-interactive
     set -Ux EDITOR nvim
     fish_vi_key_bindings
-
-    status --is-interactive; and source (rbenv init -|psub)
-    status --is-interactive; and source (pyenv init -|psub)
 
     if not set -q TMUX
         set -g TMUX tmux new-session -d -s Chore
